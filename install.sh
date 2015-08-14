9e73aaf21e4af609b4ff2ec16c9bb4c3ec52c733
Jiajun <shenjiajun90@gmail.com>
2015-08-14 10:59:22 -0500

    add prerequisites package installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install pysal
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install sklearn
+pip install shapely
+pip install https://bitbucket.org/jraedler/polygon2/downloads/Polygon2-2.0.6.zip
+pip install fiona
